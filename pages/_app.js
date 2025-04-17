0b76702f5d5225ed4a93b8584f4fecb120c219d8
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-04-16 18:46:10 -0700

    Add GA4

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,33 @@
 import '../faust.config';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { FaustProvider } from '@faustwp/core';
+import ReactGA from 'react-ga4';
+
 import 'normalize.css/normalize.css';
 import '../styles/main.scss';
-import ThemeStyles from 'components/ThemeStyles/ThemeStyles';
+import ThemeStyles from '../components/ThemeStyles/ThemeStyles';
+
+const GA_MEASUREMENT_ID = 'G-ZXQXLX3X89'; // 🔁 Replace with real GA4 ID
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    ReactGA.initialize(GA_MEASUREMENT_ID);
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+    const handleRouteChange = (url) => {
+      ReactGA.send({ hitType: "pageview", page: url });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
